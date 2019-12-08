ed492385ade52b5b1c475576156206a03f8e06a5
Ravi <rnallapu@spacePirate.com>
2019-12-07 21:23:23 -0700

    added Waypoint class to standard objects

--- anafi_swarm_ros/src/standard_objects.py
+++ anafi_swarm_ros/src/standard_objects.py
@@ -6,3 +6,22 @@ class Anafi_drone:
         self.D_IP = D_IP
         self.drone = olympe.Drone(self.D_IP, loglevel=0)
         self.drone.connection()
+
+
+class Waypoint():
+    def __init__(self, x, y, z, roll, pitch, yaw, vx, vy, vz, wx, wy, wz):
+        self.x = x
+        self.y = y
+        self.z = z
+
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
+
+        self.vx = vx
+        self.vy = vy
+        self.vz = vz
+
+        self.wx = wx
+        self.wy = wy
+        self.wz = wz
